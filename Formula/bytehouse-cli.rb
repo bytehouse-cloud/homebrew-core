3820bc110e56c7229e0b62b589eb9441be583856
kae.siew <kae.siew@bytedance.com>
2021-07-16 09:35:32 +0800

    enforce recompile on upgrade and add in test

--- Formula/bytehouse-cli.rb
+++ Formula/bytehouse-cli.rb
@@ -4,9 +4,16 @@ class BytehouseCli < Formula
   url "https://github.com/bytehouse-cloud/cli/releases/download/v1.5.3/bytehouse-darwin-amd64"
   sha256 "78cfffe4460da872ca6d1ba9d49260b1397dd5304c3a0ad9ca443762240e7b5e"
   license "Apache 2.0"
+  revision 1
+
+  bottle :unneeded
 
   def install
     File.rename("bytehouse-darwin-amd64", "bytehouse-cli")
     bin.install "bytehouse-cli"
+  end
+
+  def test
+    system "#{bin}/bytehouse-cli", "--version"
   end
 end
