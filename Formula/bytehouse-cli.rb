ba280e7ac6962577e7709939c3d9bdae550a0c84
bytehouse-devops <dpsg-devops@bytedance.com>
2021-09-21 05:34:15 +0000

    Update ByteHouse CLI HomeBrew formula with binary version v1.5.14.2

--- Formula/bytehouse-cli.rb
+++ Formula/bytehouse-cli.rb
@@ -1,15 +1,15 @@
 class BytehouseCli < Formula
   desc "ByteHouse SQL Gateway Command-Line Interface"
   homepage "https://github.com/bytehouse-cloud/cli"
-  url "https://github.com/bytehouse-cloud/cli/releases/download/v1.5.14.1/bytehouse-v1.5.14.1-darwin-amd64"
-  sha256 "5ae652196fab04e06257886257a8800b22c8f3bdc4422064e5a4e6b81ded7a95"
+  url "https://github.com/bytehouse-cloud/cli/releases/download/v1.5.14.2/bytehouse-v1.5.14.2-darwin-amd64"
+  sha256 "e69e189a0ba57ab36626ae7dc6737500ed1f66424c68be656e02372f5e48cc53"
   license "Apache 2.0"
-  version "v1.5.14.1"
+  version "v1.5.14.2"
 
   bottle :unneeded
 
   def install
-    File.rename("bytehouse-v1.5.14.1-darwin-amd64", "bytehouse-cli")
+    File.rename("bytehouse-v1.5.14.2-darwin-amd64", "bytehouse-cli")
     bin.install "bytehouse-cli"
   end
 
