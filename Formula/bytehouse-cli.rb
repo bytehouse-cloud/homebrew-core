0ca86fab174f78fd2453cebfc2a3cd8ad10ebca4
bytehouse-devops <dpsg-devops@bytedance.com>
2022-06-28 10:59:08 +0800

    Brew formula update for bytehouse-cli version v1.5.32

--- Formula/bytehouse-cli.rb
+++ Formula/bytehouse-cli.rb
@@ -5,13 +5,13 @@
 class BytehouseCli < Formula
   desc "ByteHouse SQL Gateway Command-Line Interface"
   homepage "https://github.com/bytehouse-cloud/cli"
-  version "1.5.31"
+  version "1.5.32"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/bytehouse-cloud/cli/releases/download/v1.5.31/bytehouse-cli_1.5.31_macOS_x86_64.tar.gz"
-      sha256 "d5b2effd3941c86678028bf51ba02eab6236353f0eca7cd50af76c1942a4460a"
+      url "https://github.com/bytehouse-cloud/cli/releases/download/v1.5.32/bytehouse-cli_1.5.32_macOS_x86_64.tar.gz"
+      sha256 "888bad73bf9be1ce1a198417a1b11f57b2e084b8ba493b7d794395d03dcc8889"
 
       def install
         bin.install "bytehouse-cli"
@@ -19,8 +19,8 @@ class BytehouseCli < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/bytehouse-cloud/cli/releases/download/v1.5.31/bytehouse-cli_1.5.31_macOS_arm64.tar.gz"
-      sha256 "8ab49d5b9d42f7634f4dc6b65a12de81da5f969f1b73536f61e5fa311e032d81"
+      url "https://github.com/bytehouse-cloud/cli/releases/download/v1.5.32/bytehouse-cli_1.5.32_macOS_arm64.tar.gz"
+      sha256 "22e28b0df00afbd43a5ecded1921013f83d9244116df06fe28284cc908926a6b"
 
       def install
         bin.install "bytehouse-cli"
@@ -31,8 +31,8 @@ class BytehouseCli < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bytehouse-cloud/cli/releases/download/v1.5.31/bytehouse-cli_1.5.31_Linux_x86_64.tar.gz"
-      sha256 "7b3c7f83b5ab9bbe9a4e0ef3cd0988a0bfd16f143a6ddbac983bcdcc71fad10d"
+      url "https://github.com/bytehouse-cloud/cli/releases/download/v1.5.32/bytehouse-cli_1.5.32_Linux_x86_64.tar.gz"
+      sha256 "9b4c054a525589738a0a1ecf64880890ad722c7b8fa830b5fd844057e542d41a"
 
       def install
         bin.install "bytehouse-cli"
